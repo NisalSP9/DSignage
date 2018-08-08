701f718f7e41a709438371fe8f8934240ba04b4f
nisalsp9 <nisalsp9@yahoo.com>
2018-08-08 13:14:23 +0530

    http url video done.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {VideoPlayer,VideoOptions} from "@ionic-native/video-player";
 
 @Component({
   selector: 'page-home',
@@ -7,7 +8,26 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  videoOptions: VideoOptions;
+  videoURL : string;
+
+  constructor(private videoPlayer : VideoPlayer,public navCtrl: NavController) {
+
+  }
+
+  async playVideo(){
+    try {
+
+      this.videoOptions = {
+        volume : 0.7
+      };
+      this.videoURL = "http://techslides.com/demos/sample-videos/small.mp4";
+      this.videoPlayer.play(this.videoURL,this.videoOptions);
+
+
+    }catch(e){
+      console.error(e);
+    }
 
   }
 
